66b4f8e3fad1f473d76faf7a73278c275548351a
pawanman56 <pawanman56@gmail.com>
2017-12-18 23:24:21 +0545

    Modal for subscription message was created, and styles were donr

--- javascript.js
+++ javascript.js
@@ -1,23 +1,23 @@
+//Box reveal transition on scroll
 window.sr = ScrollReveal({});
 sr.reveal('.feature-box', { duration: 2000 });
 sr.reveal('.middle-box-wrapper', { duration: 2000 });	
 sr.reveal('#header-text', { duration: 1000 });
 sr.reveal('.intro-box', { duration: 1000 });	
 
-$("li#contact").click(function() {
-    $('html, body').animate({
-        scrollTop: $("#footer").offset().top
-    }, 500);
-});
 
-$("li#about").click(function() {
-    $('html, body').animate({
-        scrollTop: $(".intro-box").offset().top
-    }, 500);
-});
-
+//Same page anchor
 $("li#features").click(function() {
     $('html, body').animate({
         scrollTop: $(".feature-box").offset().top
     }, 500);
-});	
+});
+
+
+//For successful subscription message after page redirection from mailchimp
+//var elem = document.getElementById('subscription_message'); 
+var urlParams = new URLSearchParams(window.location.search);
+
+if (urlParams.get("status") != "successful_subscription") {
+  $('#subscription_message').modal('show');
+}
